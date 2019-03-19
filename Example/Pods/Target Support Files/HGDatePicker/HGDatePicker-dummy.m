9a34dcc133002a4d7a6f22e19e7542cc87107d35
huange <fuchanghuan@wayaa.com>
2019-03-19 14:59:25 +0800

    add new pod lib

---
+++ Example/Pods/Target Support Files/HGDatePicker/HGDatePicker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HGDatePicker : NSObject
+@end
+@implementation PodsDummy_HGDatePicker
+@end
---
+++ Example/Pods/Target Support Files/Pods-HGDatePicker_Tests/Pods-HGDatePicker_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HGDatePicker_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_HGDatePicker_Tests
+@end
